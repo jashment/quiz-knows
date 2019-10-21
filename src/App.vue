d4573d2f549868bb83eef2f4296b3e978426b1da
Grant <grantshoop@gmail.com>
2019-10-20 20:22:25 -0600

    changed component name

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,18 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld />
+      <Home />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Home from './components/Home'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Home,
   },
   data: () => ({
     //
