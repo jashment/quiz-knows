a9541b979c4cb8a164b7f1ac0358173a690d519a
Jordan <jordanashment@gmail.com>
2019-11-19 18:19:21 -0700

    Getting rid of  registerServiceWorker

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
-import './registerServiceWorker'
 import vuetify from './plugins/vuetify'
 import { routes } from './routes'
 
