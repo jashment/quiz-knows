0080a337fb25d0b9638deb419662d5dd2ca0af99
Jordan <jordanashment@gmail.com>
2020-02-15 07:54:29 -0700

    Fix hopefull last error
    Had to turn off no-console and no-debugger

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,8 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:vue/recommended'],
   plugins: ['vue'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
