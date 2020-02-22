d01af2e01445396c798b0f0a40e7d516eb1a3435
Grant <grantshoop@gmail.com>
2020-02-21 23:14:20 -0700

    began work on settings

--- src/components/settings.vue
+++ src/components/settings.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <h1>Settings</h1>
+  <div class="container">
+    <h3>Full Name:</h3>
+    <h3>Display Name:</h3>
+    <h3>UVU Email:</h3>
+    <h3>Current Courses:</h3>
+    <h3>Operating System:</h3>
+    <v-btn>Edit Settings</v-btn>
+    <v-btn>Change Password</v-btn>
+  </div>
   </div>
 </template>
 
@@ -11,4 +19,19 @@ h1 {
   text-align: center;
   margin-top: 250px;
 }
+
+.container {
+  margin-top: 40px;
+  margin-left: 80px;
+}
+
+h3 {
+  margin-bottom: 7px;
+}
+
+.v-btn {
+  margin-top: 20px;
+  margin-right: 25px;
+}
+
 </style>
