c122510aee321b0c20b760a9477d29587c62d678
Grant <grantshoop@gmail.com>
2020-02-24 16:42:41 -0700

    adjustment

--- src/components/settings.vue
+++ src/components/settings.vue
@@ -1,70 +1,80 @@
 <template>
   <div>
-  <div class="container">
-    <h3>Full Name:</h3>
-    <h3>Display Name:</h3>
-    <h3>UVU Email:</h3>
-    <h3>Current Courses:</h3>
-    <h3>Operating System:</h3>
-    <!-- <v-btn>Edit Settings</v-btn> -->
-    <v-btn>Change Password</v-btn>
-    <v-dialog
-      v-model="dialog"
-      width="500"
-    >
-      <template v-slot:activator="{ on }">
-        <v-btn
-          
-          v-on="on"
-        >
-          Edit Settings
-        </v-btn>
-      </template>
-
-      <v-card>
-        <v-card-title
-          class="headline" style="background-color: #109199;"
-          primary-title
-        >
-          Edit Settings
-        </v-card-title>
-
-        <v-card-text>
-          <v-row>
-        <v-col>Full Name: <textarea></textarea></v-col>
-         <v-col>Display Name: <textarea></textarea></v-col>
-       <v-col cols="12" width="100">UVU Email: <textarea></textarea></v-col>
-        <v-col cols="12"><v-select
-         :items="courses"
-         multiple
-          label="Current Course"
-          dense
-          solo
-        ></v-select></v-col>
-         <v-col> <v-select
-          :items="items"
-          label="Operating System"
-          dense
-          solo
-        ></v-select></v-col>
-          </v-row>
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
+    <div class="container">
+      <h3>Full Name:</h3>
+      <h3>Display Name:</h3>
+      <h3>UVU Email:</h3>
+      <h3>Current Courses:</h3>
+      <h3>Operating System:</h3>
+      <!-- <v-btn>Edit Settings</v-btn> -->
+      <v-btn>Change Password</v-btn>
+      <v-dialog
+        v-model="dialog"
+        width="500"
+      >
+        <template v-slot:activator="{ on }">
           <v-btn
-            color="primary"
-            text
-            @click="dialog = false"
+          
+            v-on="on"
           >
-            Apply Changes
+            Edit Settings
           </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </div>
+        </template>
+
+        <v-card>
+          <v-card-title
+            class="headline"
+            style="background-color: #109199;"
+            primary-title
+          >
+            Edit Settings
+          </v-card-title>
+
+          <v-card-text>
+            <v-row>
+              <v-col>Full Name: <textarea /></v-col>
+              <v-col>Display Name: <textarea /></v-col>
+              <v-col
+                cols="12"
+                width="100"
+              >
+                UVU Email: <textarea />
+              </v-col>
+              <v-col cols="12">
+                <v-select
+                  :items="courses"
+                  multiple
+                  label="Current Course"
+                  dense
+                  solo
+                />
+              </v-col>
+              <v-col>
+                <v-select
+                  :items="items"
+                  label="Operating System"
+                  dense
+                  solo
+                />
+              </v-col>
+            </v-row>
+          </v-card-text>
+
+          <v-divider />
+
+          <v-card-actions>
+            <v-spacer />
+            <v-btn
+              color="primary"
+              text
+              @click="dialog = false"
+            >
+              Apply Changes
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
   </div>
 </template>
 
