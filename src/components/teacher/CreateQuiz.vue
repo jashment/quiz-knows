6c25844008ccb638ac515cd912e70ef33c6300ee
Coby Yates <cobylyates@gmail.com>
2020-03-20 15:15:02 -0600

    Started implementing Firebase on create quiz page
    - On /teacher/CreateQuiz page I added some code for the firebase to get it working
    - Not working currently

--- src/components/teacher/CreateQuiz.vue
+++ src/components/teacher/CreateQuiz.vue
@@ -12,24 +12,11 @@
       <template v-slot:top>
         <v-toolbar flat>
           <v-toolbar-title>Premeire Quiz</v-toolbar-title>
-          <v-divider
-            class="mx-4"
-            inset
-            vertical
-          />
+          <v-divider class="mx-4" inset vertical />
           <v-spacer />
-          <v-dialog
-            v-model="dialog"
-            max-width="500px"
-            persistent
-          >
+          <v-dialog v-model="dialog" max-width="500px" persistent>
             <template v-slot:activator="{ on }">
-              <v-btn
-                color="green"
-                dark
-                class="mb-2"
-                v-on="on"
-              >
+              <v-btn color="green" dark class="mb-2" v-on="on">
                 New Question
               </v-btn>
             </template>
@@ -41,11 +28,7 @@
               <v-card-text>
                 <v-container>
                   <v-row>
-                    <v-col
-                      cols="12"
-                      sm="12"
-                      md="12"
-                    >
+                    <v-col cols="12" sm="12" md="12">
                       <v-text-field
                         v-model="editedItem.name"
                         label="Question"
@@ -61,21 +44,13 @@
                     </v-col>
                   </v-row>
                   <v-row>
-                    <v-col
-                      cols="12"
-                      sm="6"
-                      md="6"
-                    >
+                    <v-col cols="12" sm="6" md="6">
                       <v-text-field
                         v-model="editedItem.macOS"
                         label="macOS Shortcut"
                       />
                     </v-col>
-                    <v-col
-                      cols="12"
-                      sm="6"
-                      md="6"
-                    >
+                    <v-col cols="12" sm="6" md="6">
                       <v-text-field
                         v-model="editedItem.windows"
                         label="Windows Shortcut"
@@ -87,17 +62,10 @@
 
               <v-card-actions>
                 <v-spacer />
-                <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="close"
-                >
+                <v-btn color="blue darken-1" text @click="close">
                   Cancel
                 </v-btn>
-                <v-btn
-                  color="blue darken-1"
-                  @click="save"
-                >
+                <v-btn color="blue darken-1" @click="save">
                   Save
                 </v-btn>
               </v-card-actions>
@@ -106,155 +74,161 @@
         </v-toolbar>
       </template>
       <template v-slot:item.action="{ item }">
-        <v-icon
-          small
-          class="mr-5 title"
-          color="blue"
-          @click="editItem(item)"
-        >
+        <v-icon small class="mr-5 title" color="blue" @click="editItem(item)">
           mdi-pencil-outline
         </v-icon>
-        <v-icon
-          small
-          class="title"
-          color="red"
-          @click="deleteItem(item)"
-        >
+        <v-icon small class="title" color="red" @click="deleteItem(item)">
           mdi-trash-can-outline
         </v-icon>
       </template>
       <template v-slot:no-data>
-        <v-btn
-          color="primary"
-          @click="initialize"
-        >
+        <v-btn color="primary" @click="initialize">
           Reset
         </v-btn>
       </template>
     </v-data-table>
-    <v-btn class="primary mb-12">
+    <v-btn class="primary mb-12" @click="saveQuiz">
       Save Quiz
     </v-btn>
   </v-container>
 </template>
 
 <script>
+import firebase from "firebase";
+
 export default {
   data: () => ({
     dialog: false,
     headers: [
-      { text: 'Question', align: 'left', sortable: false, value: 'name' },
-      { text: 'macOS', value: 'macOS', sortable: false },
-      { text: 'Windows', value: 'windows', sortable: false },
-      { text: 'Actions', value: 'action', sortable: false },
+      { text: "Question", align: "left", sortable: false, value: "name" },
+      { text: "macOS", value: "macOS", sortable: false },
+      { text: "Windows", value: "windows", sortable: false },
+      { text: "Actions", value: "action", sortable: false }
     ],
     questions: [],
     editedIndex: -1,
     editedItem: {
-      name: '',
-      macOS: '',
-      windows: '',
+      name: "",
+      macOS: "",
+      windows: ""
     },
     defaultItem: {
-      name: '',
-      macOS: '',
-      windows: '',
+      name: "",
+      macOS: "",
+      windows: ""
     },
+    quiz: [
+      {
+        details: {
+          description: "test",
+          software: "Premeiere"
+        },
+        questions: [{ question: "thequestion", answer: "theanswer" }]
+      }
+    ]
   }),
 
   computed: {
     formTitle() {
-      return this.editedIndex === -1 ? 'New Question' : 'Edit Question'
-    },
+      return this.editedIndex === -1 ? "New Question" : "Edit Question";
+    }
   },
 
   watch: {
     dialog(val) {
-      val || this.close()
-    },
+      val || this.close();
+    }
   },
 
   created() {
-    this.initialize()
+    this.initialize();
   },
 
   methods: {
+    saveQuiz() {
+      console.log(this.quiz);
+      firebase
+        .database()
+        .ref("quizzes/")
+        .set({
+          quiz: this.quiz
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
     initialize() {
       this.questions = [
         {
-          name: 'How do you make an edit wherever your playhead is located?',
-          macOS: 'Cmd, K',
-          windows: 'Ctrl, K',
+          name: "How do you make an edit wherever your playhead is located?",
+          macOS: "Cmd, K",
+          windows: "Ctrl, K"
         },
         {
           name:
-            'Pressing ___ while your playhead is over an existing Marker will bring up the Marker dialog box',
-          macOS: 'M',
-          windows: 'M',
+            "Pressing ___ while your playhead is over an existing Marker will bring up the Marker dialog box",
+          macOS: "M",
+          windows: "M"
         },
         {
           name:
-            'What key do you press to locate a source clip from within your timeline?',
-          macOS: 'F',
-          windows: 'F',
+            "What key do you press to locate a source clip from within your timeline?",
+          macOS: "F",
+          windows: "F"
         },
         {
-          name: 'What keys bring up the Export Media dialog box?',
-          macOS: 'Cmd, M',
-          windows: 'Ctrl, M',
+          name: "What keys bring up the Export Media dialog box?",
+          macOS: "Cmd, M",
+          windows: "Ctrl, M"
         },
         {
-          name: 'What key is for making In point on a clip?',
-          macOS: 'I',
-          windows: 'I',
+          name: "What key is for making In point on a clip?",
+          macOS: "I",
+          windows: "I"
         },
         {
-          name: 'What key is for making Out point on a clip?',
-          macOS: 'O',
-          windows: 'O',
+          name: "What key is for making Out point on a clip?",
+          macOS: "O",
+          windows: "O"
         },
         {
-          name: 'What key makes your video playback at a faster speed?',
-          macOS: 'L',
-          windows: 'L',
-        },
-      ]
+          name: "What key makes your video playback at a faster speed?",
+          macOS: "L",
+          windows: "L"
+        }
+      ];
     },
 
     editItem(item) {
-      this.editedIndex = this.questions.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
-    },
-
-    saveQuiz() {
-      //save the quiz to the database
+      this.editedIndex = this.questions.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
     },
 
     deleteItem(item) {
-      const index = this.questions.indexOf(item)
-      confirm('Are you sure you want to delete this question?') &&
-        this.questions.splice(index, 1)
+      const index = this.questions.indexOf(item);
+      confirm("Are you sure you want to delete this question?") &&
+        this.questions.splice(index, 1);
     },
 
     close() {
-      this.dialog = false
+      this.dialog = false;
       setTimeout(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      }, 300)
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      }, 300);
     },
 
     save() {
       if (this.editedIndex > -1) {
-        Object.assign(this.questions[this.editedIndex], this.editedItem)
+        Object.assign(this.questions[this.editedIndex], this.editedItem);
       } else {
-        this.questions.push(this.editedItem)
+        this.questions.push(this.editedItem);
       }
-      this.close()
-    },
-  },
-}
+      this.close();
+    }
+  }
+};
 </script>
 
 <style></style>
