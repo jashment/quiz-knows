906351709e6862a42853ffa17069c37709190091
Coby Yates <cobylyates@gmail.com>
2020-03-24 18:17:03 -0600

    Everything working on CreateQuiz Page - All logic working, just need to uncomment DB update

--- src/components/teacher/CreateQuiz.vue
+++ src/components/teacher/CreateQuiz.vue
@@ -8,17 +8,17 @@
         <v-card class="pa-10 elevation-8">
           <p class="title">Quiz Details</p>
           <v-text-field 
-            label="Quiz Title" 
+            clearable label="Quiz Title" 
             placeholder="premiere practice" 
             v-model="quiz.details.title"></v-text-field>
           <v-text-field 
-            label="Software" 
+            clearable label="Software" 
             placeholder="ex: adobe premiere" 
             v-model="quiz.details.software"></v-text-field>
           <v-textarea 
-            clearable label="ex: Quiz Description" 
+            clearable label="Quiz Description" 
             rows="3" 
-            placeholder="ex: Use shortcut keys from the Adobe Premiere program to answer the quiz questions." 
+            placeholder="example: Use shortcut keys from the Adobe Premiere program to answer the quiz questions." 
             v-model="quiz.details.description"></v-textarea>
         </v-card>
 
@@ -51,17 +51,12 @@
                       <v-textarea
                         v-model="editedItem.question"
                         dense
+                        clearable
                         label="Question"
                       />
                     </v-col>
                   </v-row>
                   <v-row>
-                    <v-col class="text-center pb-0">
-                      <p>
-                        Enter keys separated by a comma. (example: cmd, shift,
-                        t)
-                      </p>
-                    </v-col>
                   </v-row>
                   <v-row>
                     <v-col cols="12" sm="6" md="6">
@@ -74,6 +69,7 @@
                     <v-col cols="12" sm="6" md="6">
                       <v-text-field
                         v-model="editedItem.windows"
+                        @keydown="logKeyW"
                         label="Windows Shortcut"
                       />
                     </v-col>
@@ -119,7 +115,8 @@
 export default {
   data: () => ({
     dialog: false,
-    keystroke: [],
+    keystrokeMac: [],
+    keystrokeWin: [],
     headers: [
       { text: "Question", align: "left", sortable: false, value: "question" },
       { text: "macOS", value: "macOS", sortable: false },
@@ -170,14 +167,42 @@ export default {
   methods: {
     logKey: function(event) {
       event.preventDefault()
-      this.keystroke.push(event.key)
-      this.editedItem.macOS = this.keystroke
-      console.log(this.keystroke)
+      if (event.key != 'Backspace'){
+        if (event.key === ' ') {
+          this.keystrokeMac.push(event.code)
+        }
+        else {
+          this.keystrokeMac.push(event.key)
+        }
+      }
+      else if (event.key === 'Backspace') {
+        this.keystrokeMac.pop()
+      }
+      this.editedItem.macOS = this.keystrokeMac
+      console.log(this.keystrokeMac)
+      console.log(event.key)
+    },
+    logKeyW: function(event) {
+      event.preventDefault()
+      if (event.key != 'Backspace'){
+        if (event.key === ' ') {
+          this.keystrokeWin.push(event.code)
+        }
+        else {
+          this.keystrokeWin.push(event.key)
+        }
+      }
+      else if (event.key === 'Backspace') {
+        this.keystrokeWin.pop()
+      }
+      this.editedItem.windows = this.keystrokeWin
+      console.log(this.keystrokeWin)
       console.log(event.key)
     },
     saveQuiz() {
-      console.log( this.quiz.details );
-      console.log( this.quiz.questions );
+      // console.log( this.quiz.details );
+      // console.log( this.quiz.questions );
+      console.log(this.quiz)
       /*
       firebase
         .database()
@@ -196,7 +221,8 @@ export default {
 
     editItem(item) {
       this.editedIndex = this.quiz.questions.indexOf(item);
-      this.editedItem.macOS = this.keystroke
+      this.editedItem.macOS = this.keystrokeMac
+      this.editedItem.windows = this.keystrokeWin
       this.editedItem = Object.assign({}, item);
       this.dialog = true;
     },
@@ -213,7 +239,8 @@ export default {
         this.editedItem = Object.assign({}, this.defaultItem);
         this.editedIndex = -1;
       }, 300);
-      this.keystroke = []
+      this.keystrokeMac = []
+      this.keystrokeWin = []
     },
 
     save() {
